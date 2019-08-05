5ab2a97b8949ebad1c73f2ed8962dce2635cdddf
Nathan Van Wyck <nathanvanwyck@Macintosh-00254bbd8f8a-2.local>
2019-08-05 19:55:56 -0400

    Fixed mistake

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lambdata_pkg_nrvanwyck",
-    version="0.0.1",
+    version="0.0.2",
     author="Nathan Van Wyck",
     author_email="nathanvanwyck@gmail.com",
     description="This simple package has a function to check nulls in a pandas DataFrame and a function to plot a labeled confusion matrix.",
