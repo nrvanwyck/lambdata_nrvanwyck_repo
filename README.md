594cec6b520fda1132486478d62c0478862ff061
Nathan Van Wyck <nathanvanwyck@Macintosh-00254bbd8f8a-2.local>
2019-08-05 19:00:54 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This simple package has a function to check nulls in a pandas DataFrame and a function to plot a labeled confusion matrix.
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lambdata_nrvanwyck",
+    version="0.0.1",
+    author="Nathan Van Wyck",
+    author_email="nathanvanwyck@gmail.com",
+    description="This simple package has a function to check nulls in a pandas DataFrame and a function to plot a labeled confusion matrix.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nrvanwyck/lambdata_nrvanwyck",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
